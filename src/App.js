e54223e4a73c09b29407e93572f14cb1e61abb5b
Trong Nghia <trongnghiaa.work@gmail.com>
2024-04-23 11:15:02 +0700

    Re-commit
    Testing Gemini-api.
    Error: Generate result.

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import GeminiTest from './GeminiTest';
+
+function App() {
+  return (
+    <div className="App">
+      <GeminiTest />
+    </div>
+  );
+}
+
+export default App;
