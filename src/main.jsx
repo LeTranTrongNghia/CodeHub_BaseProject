2d14e605906d5525ed2d03555087230b63abe717
Trong Nghia <trongnghiaa.work@gmail.com>
2024-04-26 18:46:48 +0700

    Login Page UI

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,12 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
+import App from './main/App.jsx'
 import './index.css'
+import Login from './Login/login.jsx'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
+    <Login />
     <App />
   </React.StrictMode>,
 )
