4ae480d592e4c06d807a626788f90473384038cc
Trong Nghia <trongnghiaa.work@gmail.com>
2024-04-30 14:25:07 +0700

    Update Folder Structure

---
+++ src/container/Home/pages/UserHome/UserHome.jsx
@@ -0,0 +1,5 @@
+const UserHome = () => {
+	return <div className='text-red-500 text-2xl'>UserHome</div>;
+};
+
+export default UserHome;
--- src/main.jsx
+++ src/main.jsx
@@ -4,16 +4,11 @@ import { unstable_HistoryRouter as HistoryBrowserRouter } from 'react-router-dom
 import App from './App.jsx';
 import { createBrowserHistory } from 'history';
 import './index.css';
-import Login from './container/Auth/Login';
-import CodeEditorWrapper from './Code_Editor/code_editor.jsx';
-import AI_chat from './main/AI_chat.jsx';
 
 export const history = createBrowserHistory();
 
 ReactDOM.createRoot(document.getElementById('root')).render(
 	<React.StrictMode>
-		{/* <AI_chat /> */}
-		{/* <CodeEditorWrapper /> */}
 		<HistoryBrowserRouter history={history}>
 			<App />
 		</HistoryBrowserRouter>
