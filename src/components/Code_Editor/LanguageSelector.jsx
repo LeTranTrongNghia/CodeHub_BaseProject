f1367a72e36d10f5e7f6a9b0571223b9cdfe8d66
Trong Nghia <trongnghiaa.work@gmail.com>
2024-05-01 14:04:10 +0700

    Update Workspace UI

--- src/components/Code_Editor/LanguageSelector.jsx
+++ src/components/Code_Editor/LanguageSelector.jsx
@@ -48,15 +48,6 @@ const LanguageSelector = ({ onSelect }) => {
 					))}
 				</ul>
 			)}
-
-			{/* AI Assistant */}
-			<button
-				type='submit'
-				className='bg-blue-300 p-3 rounded-md hover:bg-blue-400 transition-all duration-300 mx-2text-left px-4 py-2 rounded-md focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-blue-500 bg-gray-900 text-white ml-2'
-				onClick={() => navigate('/ai-assistant')}
-			>
-				AI Assistant
-			</button>
 		</div>
 	);
 };
--- src/container/Workspace/Code_Editor/code_editor.jsx
+++ src/container/Workspace/Code_Editor/code_editor.jsx
@@ -4,6 +4,7 @@ import * as monaco from 'monaco-editor'; // Import monaco-editor
 import LanguageSelector from "/src/components/Code_Editor/LanguageSelector";
 import Output from "/src/components/Code_Editor/Output";
 import ProblemDescription from "/src/components/Code_Editor/ProblemDescription";
+import AI_chat from "/src/container/Workspace/AI_chat/AI_chat";
 import { CODE_SNIPPETS } from './constant/constants';
 import { useNavigate } from 'react-router-dom';
 
@@ -11,6 +12,7 @@ const CodeEditorWrapper = () => {
 	const navigate = useNavigate();
 	const [value, setValue] = useState('');
 	const [language, setLanguage] = useState('');
+	const [option, setOption] = useState('1');
 	const editorRef = useRef();
 
 	const onSelect = language => {
@@ -37,7 +39,24 @@ const CodeEditorWrapper = () => {
 				>
 					Back to Homepage
 				</button>
-				<ProblemDescription />
+				<button
+					type='submit'
+					className={`option ${option === '1' ? 'selected' : ''} bg-blue-300 p-3 rounded-md hover:bg-blue-400 transition-all duration-300 mx-2 text-left px-4 py-2 rounded-md focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-blue-500 bg-gray-900 text-white ml-4`}
+					onClick={(e) => setOption('1')}
+				>
+					Code Editor
+				</button>
+				<button
+					type='submit'
+					className={`option ${option === '2' ? 'selected' : ''} bg-blue-300 p-3 rounded-md hover:bg-blue-400 transition-all duration-300 mx-2 text-left px-4 py-2 rounded-md focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-blue-500 bg-gray-900 text-white ml-4`}
+					onClick={(e) => setOption('2')}
+				>
+					AI Assistant
+				</button>
+				<div>
+					{option === '1' && <div><ProblemDescription /></div>}
+					{option === '2' && <div><AI_chat /></div>}
+				</div>
 			</div>
 
 			<div className='w-2/5 bg-gray-800 text-gray-300 p-4 rounded-md'>
@@ -45,7 +64,7 @@ const CodeEditorWrapper = () => {
 				<Editor
 					height='58vh'
 					defaultLanguage={language}
-					defaultValue= {CODE_SNIPPETS[language]}
+					defaultValue={CODE_SNIPPETS[language]}
 					theme='vs-dark'
 					onMount={editor => {
 						editorRef.current = editor;
