60195950124c47a54dff8990c2d10b77fc5303fc
Lê Phạm Ngọc Uyển <ngocuyenlepham@gmail.com>
2024-05-04 18:59:16 +0700

    feat: add reset password page

--- src/container/Auth/ChangePassword/ChangePassword.jsx
+++ src/container/Auth/ChangePassword/ChangePassword.jsx
@@ -28,9 +28,9 @@ const ChangePassword = () => {
 									</label>
 									<div className='mt-1'>
 										<input
-											id='email'
-											name='email'
-											type='email'
+											id='password'
+											name='password'
+											type='password'
 											autocomplete='email'
 											required
 											className='appearance-none rounded-md relative block w-full px-3 py-2 border border-gray-300 placeholder-gray-500 text-gray-900 focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 focus:z-10 sm:text-sm'
