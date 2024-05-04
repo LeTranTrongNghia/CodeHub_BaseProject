ac5a6f273c8bf656c2e5961bf674434d7224dda6
Lê Phạm Ngọc Uyển <ngocuyenlepham@gmail.com>
2024-05-04 14:03:28 +0700

    feat: add register page

--- src/container/Auth/Register/Register.jsx
+++ src/container/Auth/Register/Register.jsx
@@ -1,5 +1,158 @@
+import { useNavigate } from 'react-router-dom';
+
 const Register = () => {
-	return <div>Register</div>;
+	const navigate = useNavigate();
+	return (
+		<>
+			<div
+				className='min-h-screen bg-cover bg-center'
+				style={{
+					backgroundImage: `url(https://images.pexels.com/photos/7134986/pexels-photo-7134986.jpeg)`,
+				}}
+			>
+				<div className='min-h-screen  flex flex-col justify-center py-12 sm:px-6 lg:px-8'>
+					<div className='sm:mx-auto sm:w-full sm:max-w-md'>
+						<h2 className='mt-6 text-center text-3xl font-extrabold text-gray-900'>
+							Create a new account
+						</h2>
+						<p className='mt-2 text-center text-sm text-gray-600 max-w'>
+							Or
+							<a
+								onClick={() => navigate('/login')}
+								className='font-medium text-blue-600 hover:text-blue-500 pl-1 cursor-pointer'
+							>
+								Login
+							</a>
+						</p>
+					</div>
+
+					<div className='mt-6 sm:mx-auto sm:w-full sm:max-w-md'>
+						<div className='bg-white py-8 px-4 shadow sm:rounded-lg sm:px-10'>
+							<form>
+								<div className='py-2'>
+									<label
+										htmlFor='email'
+										className='block text-sm font-medium text-gray-700'
+									>
+										Email
+									</label>
+									<div className='mt-1'>
+										<input
+											id='email'
+											name='email'
+											type='email'
+											autocomplete='email'
+											required
+											className='appearance-none rounded-md relative block w-full px-3 py-2 border border-gray-300 placeholder-gray-500 text-gray-900 focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 focus:z-10 sm:text-sm'
+											placeholder='Enter your email address'
+										/>
+									</div>
+								</div>
+
+								<div className='py-2'>
+									<label
+										for='password'
+										className='block text-sm font-medium text-gray-700'
+									>
+										Password
+									</label>
+									<div className='mt-1'>
+										<input
+											id='password'
+											name='password'
+											type='password'
+											autocomplete='current-password'
+											required
+											className='appearance-none rounded-md relative block w-full px-3 py-2 border border-gray-300 placeholder-gray-500 text-gray-900 focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 focus:z-10 sm:text-sm'
+											placeholder='Enter your password'
+										/>
+									</div>
+								</div>
+
+								<div className='py-2'>
+									<label
+										for='password'
+										className='block text-sm font-medium text-gray-700'
+									>
+										Confirm Password
+									</label>
+									<div className='mt-1'>
+										<input
+											id='password'
+											name='password'
+											type='password'
+											autocomplete='current-password'
+											required
+											className='appearance-none rounded-md relative block w-full px-3 py-2 border border-gray-300 placeholder-gray-500 text-gray-900 focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 focus:z-10 sm:text-sm'
+											placeholder='Enter your password'
+										/>
+									</div>
+								</div>
+
+								<button
+									type='submit'
+									className='group relative w-full flex justify-center mt-5 py-2 px-4 border border-transparent text-sm font-medium rounded-md text-white bg-indigo-600 hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500'
+								>
+									Sign up
+								</button>
+							</form>
+							<div className='mt-6'>
+								<div className='relative'>
+									<div className='absolute inset-0 flex items-center'>
+										<div className='w-full border-t border-gray-300'></div>
+									</div>
+									<div className='relative flex justify-center text-sm'>
+										<span className='px-2 bg-white text-gray-500'>
+											Or continue with
+										</span>
+									</div>
+								</div>
+
+								<div className='mt-6 grid grid-cols-3 gap-3'>
+									<div>
+										<a
+											href='#'
+											className='w-full flex items-center justify-center px-8 py-3 border border-gray-300 rounded-md shadow-sm text-sm font-medium text-gray-700 bg-white hover:bg-gray-50'
+										>
+											<img
+												className='h-5 w-5'
+												src='https://www.svgrepo.com/show/512120/facebook-176.svg'
+												alt=''
+											/>
+										</a>
+									</div>
+									<div>
+										<a
+											href='#'
+											className='w-full flex items-center justify-center px-8 py-3 border border-gray-300 rounded-md shadow-sm text-sm font-medium text-gray-700 bg-white hover:bg-gray-50'
+										>
+											<img
+												className='h-5 w-5'
+												src='https://www.svgrepo.com/show/512317/github-142.svg'
+												alt=''
+											/>
+										</a>
+									</div>
+									<div>
+										<a
+											href='#'
+											className='w-full flex items-center justify-center px-8 py-3 border border-gray-300 rounded-md shadow-sm text-sm font-medium text-gray-700 bg-white hover:bg-gray-50'
+										>
+											<img
+												className='h-5 w-5'
+												src='https://www.svgrepo.com/show/506498/google.svg'
+												alt=''
+											/>
+										</a>
+									</div>
+								</div>
+							</div>
+						</div>
+					</div>
+				</div>
+			</div>
+		</>
+	);
 };
 
 export default Register;
