44e2d1bb0099a0463b5699fb46300559070822d9
Lê Phạm Ngọc Uyển <ngocuyenlepham@gmail.com>
2024-05-05 01:40:48 +0700

    feat: add validation for change password form

--- src/container/Auth/ChangePassword/ChangePassword.jsx
+++ src/container/Auth/ChangePassword/ChangePassword.jsx
@@ -1,3 +1,5 @@
+import { Alert, Button, Form, Input } from 'antd';
+
 const ChangePassword = () => {
 	return (
 		<>
@@ -15,75 +17,159 @@ const ChangePassword = () => {
 					</div>
 
 					<div className='mt-6 sm:mx-auto sm:w-full sm:max-w-md'>
-						<div className='bg-white py-8 px-4 shadow sm:rounded-lg sm:px-10'>
-							<form className='py-2'>
-								<div className='py-2'>
+						<div className='bg-white py-6 px-4 shadow sm:rounded-lg sm:px-10'>
+							<Form>
+								<div>
 									<label
-										htmlFor='email'
+										htmlFor='password'
 										className='block text-sm font-medium text-gray-700'
 									>
 										Old password
 									</label>
-									<div className='mt-1'>
-										<input
-											id='password'
-											name='password'
-											type='password'
-											autocomplete='email'
-											required
+									<Form.Item
+										name='old_password'
+										className='mt-1'
+										rules={[
+											{
+												type: 'password',
+												message: (
+													<Alert
+														className='ml-2 bg-transparent text-sm text-red-700'
+														message='invalid password'
+														banner
+														type='error'
+													/>
+												),
+											},
+											{
+												required: true,
+												message: (
+													<Alert
+														className='ml-2 bg-transparent text-sm text-red-700'
+														message='please enter old password'
+														banner
+														type='error'
+													/>
+												),
+											},
+										]}
+									>
+										<Input
 											className='appearance-none rounded-md relative block w-full px-3 py-2 border border-gray-300 placeholder-gray-500 text-gray-900 focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 focus:z-10 sm:text-sm'
-											placeholder='Enter your email address'
+											classNames={{
+												input: 'text-md font-normal',
+											}}
+											placeholder='Enter your old password'
 										/>
-									</div>
+									</Form.Item>
 								</div>
 
-								<div className='py-2'>
+								<div>
 									<label
-										for='password'
+										htmlFor='password'
 										className='block text-sm font-medium text-gray-700'
 									>
 										New password
 									</label>
-									<div className='mt-1'>
-										<input
-											id='password'
-											name='password'
-											type='password'
-											autocomplete='current-password'
-											required
+									<Form.Item
+										name='new_password'
+										className='mt-1'
+										rules={[
+											{
+												type: 'password',
+												message: (
+													<Alert
+														className='ml-2 bg-transparent text-sm text-red-700'
+														message='invalid password'
+														banner
+														type='error'
+													/>
+												),
+											},
+											{
+												required: true,
+												message: (
+													<Alert
+														className='ml-2 bg-transparent text-sm text-red-700'
+														message='please enter new password'
+														banner
+														type='error'
+													/>
+												),
+											},
+										]}
+									>
+										<Input
 											className='appearance-none rounded-md relative block w-full px-3 py-2 border border-gray-300 placeholder-gray-500 text-gray-900 focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 focus:z-10 sm:text-sm'
-											placeholder='Enter your password'
+											classNames={{
+												input: 'text-md font-normal',
+											}}
+											placeholder='Enter your new password'
 										/>
-									</div>
+									</Form.Item>
 								</div>
 
-								<div className='py-2'>
+								<div>
 									<label
-										for='password'
+										htmlFor='password'
 										className='block text-sm font-medium text-gray-700'
 									>
 										Confirm new password
 									</label>
-									<div className='mt-1'>
-										<input
-											id='password'
-											name='password'
-											type='password'
-											autocomplete='current-password'
-											required
+									<Form.Item
+										name='confirm_password'
+										className='mt-1'
+										rules={[
+											{
+												required: true,
+												message: (
+													<Alert
+														className='ml-2 bg-transparent text-sm text-red-700'
+														message='please enter confirm password'
+														banner
+														type='error'
+													/>
+												),
+											},
+											({ getFieldValue }) => ({
+												validator(_, value) {
+													if (
+														!value ||
+														getFieldValue('new_password') === value
+													) {
+														return Promise.resolve();
+													}
+													return Promise.reject(
+														<Alert
+															className='ml-2 bg-transparent text-sm text-red-700'
+															message='The new password that you entered do not match!'
+															banner
+															type='error'
+														/>,
+													);
+												},
+											}),
+										]}
+									>
+										<Input
 											className='appearance-none rounded-md relative block w-full px-3 py-2 border border-gray-300 placeholder-gray-500 text-gray-900 focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 focus:z-10 sm:text-sm'
-											placeholder='Enter your password'
+											classNames={{
+												input: 'text-md font-normal',
+											}}
+											placeholder='Enter your confirm password'
 										/>
-									</div>
+									</Form.Item>
 								</div>
 
-								<button
-									type='submit'
-									className='group relative w-full flex justify-center mt-5 py-2 px-4 border border-transparent text-sm font-medium rounded-md text-white bg-indigo-600 hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500'
-								>
-									SUBMIT
-								</button>
-							</form>
+								<Form.Item>
+									<Button
+										htmlType='submit'
+										className='btn-submit w-full p-0 mt-4 border border-transparent text-sm font-medium rounded-md text-white bg-indigo-600 hover:bg-indigo-700 focus:outline-none focus:ring-offset-2 focus:ring-indigo-500'
+									>
+										Sign up
+									</Button>
+								</Form.Item>
+							</Form>
 						</div>
 					</div>
 				</div>
