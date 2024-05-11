6c985fe92b5e20a9c3e4ac38bd48b713064d9cf8
Trong Nghia <trongnghiaa.work@gmail.com>
2024-05-11 15:33:27 +0700

    Update HomePage
    Create Courses page and Lectures page.

--- src/main.jsx
+++ src/main.jsx
@@ -4,10 +4,14 @@ import { unstable_HistoryRouter as HistoryBrowserRouter } from 'react-router-dom
 import App from './App.jsx';
 import { createBrowserHistory } from 'history';
 import './index.css';
+import ProblemsPage from './container/Home/pages/MainHome/ProblemsPage.jsx';
+import CoursesPage from './container/Home/pages/MainHome/CoursesPage.jsx';
+import LecturesPage from './container/Home/pages/MainHome/LecturesPage.jsx';
 
 export const history = createBrowserHistory();
 
 ReactDOM.createRoot(document.getElementById('root')).render(
+	// <ProblemsPage />
 	<React.StrictMode>
 		<HistoryBrowserRouter history={history}>
 			<App />
