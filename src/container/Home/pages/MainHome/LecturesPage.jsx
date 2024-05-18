892d05a8649fe7724f1495c2b3e5c27d18637938
Lê Phạm Ngọc Uyển <ngocuyenlepham@gmail.com>
2024-05-18 10:13:47 +0700

    feat: route index home & fix: navigate topbar

--- src/container/Home/pages/MainHome/LecturesPage.jsx
+++ src/container/Home/pages/MainHome/LecturesPage.jsx
@@ -1,35 +1,41 @@
-import { Button } from "@/components/ui/button"
+import { Button } from '@/components/ui/button';
 import React from 'react';
-import Sidebar from "@/components/MainHome/Sidebar";
-import Topbar from "@/components/MainHome/Topbar";
+import Sidebar from '@/components/MainHome/Sidebar';
+import Topbar from '@/components/MainHome/Topbar';
 import {
-    Card,
-    CardContent,
-    CardDescription,
-    CardHeader,
-    CardTitle,
-} from "@/components/ui/card"
+	Card,
+	CardContent,
+	CardDescription,
+	CardHeader,
+	CardTitle,
+} from '@/components/ui/card';
 import {
-    Tooltip,
-    TooltipContent,
-    TooltipProvider,
-    TooltipTrigger,
-} from "@/components/ui/tooltip";
+	Tooltip,
+	TooltipContent,
+	TooltipProvider,
+	TooltipTrigger,
+} from '@/components/ui/tooltip';
 
 const LecturesPage = () => {
-
-    return <div className="flex min-h-screen w-full flex-col bg-black">
-        {/* Topbar */}
-        <Topbar />
-        {/* Sidebar */}
-        <Sidebar />
-        {/* Mainbar */}
-        <main className="gap-4 p-4 md:gap-8 md:p-8 ml-16">
-            <div class="aspect-w-16 aspect-h-9 flex w-full">
-                <iframe src="https://www.youtube.com/embed/jS4aFq5-91M" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
-            </div>
-        </main>
-    </div>
-}
+	return (
+		<div className='flex min-h-screen w-full flex-col bg-black'>
+			{/* Topbar */}
+			<Topbar />
+			{/* Sidebar */}
+			<Sidebar />
+			{/* Mainbar */}
+			<main className='gap-4 p-4 md:gap-8 md:p-8 ml-16'>
+				<div class='aspect-w-16 aspect-h-9 flex w-full'>
+					<iframe
+						src='https://www.youtube.com/embed/jS4aFq5-91M'
+						frameborder='0'
+						allow='accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture'
+						allowfullscreen
+					></iframe>
+				</div>
+			</main>
+		</div>
+	);
+};
 
 export default LecturesPage;
