94228114aa4e2950ef8047198347e49ec11e79a8
Trong Nghia <trongnghiaa.work@gmail.com>
2024-07-11 11:38:02 +0700

    Testcase array error

--- app/src/components/RecordList.jsx
+++ app/src/components/RecordList.jsx
@@ -16,7 +16,7 @@ const Record = (props) => (
       <div className="flex gap-2">
         <Link
           className="inline-flex items-center justify-center whitespace-nowrap text-sm font-medium ring-offset-background transition-colors focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 disabled:pointer-events-none disabled:opacity-50 border border-input bg-background hover:bg-slate-100 h-9 rounded-md px-3"
-          to={`/edit/${props.record._id}`}
+          to={`/records/edit/${props.record._id}`}
         >
           Edit
         </Link>
