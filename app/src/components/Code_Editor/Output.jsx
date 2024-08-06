57efa8703f65342c9b6b77fba56a9e44fbb279d0
Trong Nghia <trongnghiaa.work@gmail.com>
2024-08-06 14:20:54 +0700

    Update Workspace & Playground
    Workspace: Remake submit work.
    Playground: Smooth animation for overall review chart.

--- app/src/components/Code_Editor/Output.jsx
+++ app/src/components/Code_Editor/Output.jsx
@@ -1,7 +1,30 @@
-import React, { useState, useEffect } from 'react';
+import {
+	Info,
+} from "lucide-react";
 import { executeCode } from '/src/container/Workspace/Code_Editor/constant/api';
 import { toast } from 'react-toastify';
 import { useParams, useNavigate } from 'react-router-dom';
+import { Button } from "@/components/ui/button";
+import {
+	Dialog,
+	DialogContent,
+	DialogDescription,
+	DialogHeader,
+	DialogTitle,
+	DialogTrigger,
+} from "@/components/ui/dialog";
+import {
+	TooltipProvider,
+	Tooltip,
+	TooltipContent,
+	TooltipTrigger,
+} from "@/components/ui/tooltip";
+import { Label } from "@/components/ui/label";
+import { PolarAngleAxis, RadialBar, RadialBarChart } from "recharts";
+import { ChartContainer } from "@/components/ui/chart";
+import { Card, CardContent } from "@/components/ui/card";
+import React, { useState, useEffect } from 'react';
+import axios from 'axios';
 
 const Output = ({ editorRef, language }) => {
 	const [form, setForm] = useState({
@@ -25,6 +48,11 @@ const Output = ({ editorRef, language }) => {
 	const [passedTests, setPassedTests] = useState(0);
 	const params = useParams();
 	const navigate = useNavigate();
+	const [data, setData] = useState([]);
+	const [correctnessRating, setCorrectnessRating] = useState(0);
+	const [performanceRating, setPerformanceRating] = useState(0);
+	const [clarityRating, setClarityRating] = useState(0);
+	const [generatingAnswer, setGeneratingAnswer] = useState(false);
 
 	useEffect(() => {
 		async function fetchData() {
@@ -47,12 +75,16 @@ const Output = ({ editorRef, language }) => {
 		fetchData();
 	}, [params.id, navigate]);
 
+	const [executionTime, setExecutionTime] = useState(null);
+
 	const runCode = async () => {
 		const sourceCode = editorRef.current.getValue();
 		if (!sourceCode) return;
 
 		setIsLoading(true);
 		setErrorMessage(null);
+
+		const startTime = Date.now();
 
 		try {
 			const { run: result } = await executeCode(language, sourceCode);
@@ -62,18 +94,47 @@ const Output = ({ editorRef, language }) => {
 			if (passed === form.testCases.length) {
 				toast.success(`Congratulations! All ${passed} test cases passed! 🎉`);
 			} else {
-				toast.info(
-					`Your code passed ${passed} out of ${form.testCases.length} test cases.`,
-				);
+				toast.error(`Your code passed ${passed} out of ${form.testCases.length} test cases.`);
 			}
 		} catch (error) {
 			console.error(error);
 			setIsError(true);
 			setErrorMessage('An error occurred while running your code.');
 		} finally {
+			const endTime = Date.now();
+			const time = endTime - startTime;
+			setExecutionTime(time);
+
 			setIsLoading(false);
 		}
 	};
+	// const runCode = async () => {
+	// 	const sourceCode = editorRef.current.getValue();
+	// 	if (!sourceCode) return;
+
+	// 	setIsLoading(true);
+	// 	setErrorMessage(null);
+
+	// 	try {
+	// 		const { run: result } = await executeCode(language, sourceCode);
+	// 		const userOutput = result.output.split('\n');
+	// 		setOutput(userOutput);
+	// 		const passed = checkTestcase(userOutput);
+	// 		if (passed === form.testCases.length) {
+	// 			toast.success(`Congratulations! All ${passed} test cases passed! 🎉`);
+	// 		} else {
+	// 			toast.error(
+	// 				`Your code passed ${passed} out of ${form.testCases.length} test cases.`,
+	// 			);
+	// 		}
+	// 	} catch (error) {
+	// 		console.error(error);
+	// 		setIsError(true);
+	// 		setErrorMessage('An error occurred while running your code.');
+	// 	} finally {
+	// 		setIsLoading(false);
+	// 	}
+	// };
 
 	const checkTestcase = userOutput => {
 		const sourceCode = editorRef.current.getValue();
@@ -114,6 +175,137 @@ const Output = ({ editorRef, language }) => {
 		}
 	};
 
+	const requestRating = async () => {
+		const sourceCode = editorRef.current.getValue();
+		setGeneratingAnswer(true);
+		console.log('Requesting rating...');
+
+		try {
+			const response = await axios({
+				url: `https://generativelanguage.googleapis.com/v1beta/models/gemini-pro:generateContent?key=${import.meta.env.VITE_API_GENERATIVE_LANGUAGE_CLIENT}`,
+				method: 'post',
+				data: {
+					contents: [
+						{
+							parts: [
+								{
+									text: `Rate the following code on a scale from 1 to 100 for the following criteria:\n\nCorrectness\nPerformance\nClarity\nGive me the answer of numbers in order only, for example: 90\n70\n80\n.\n\nCode:\n${sourceCode}`,
+								},
+							],
+						},
+					],
+				},
+			});
+
+			const ratingsText = response.data.candidates[0]?.content?.parts[0]?.text?.trim();
+			if (ratingsText) {
+				const ratings = ratingsText.split('\n');
+				if (ratings.length === 3) {
+					setCorrectnessRating(Number(ratings[0]));
+					setPerformanceRating(Number(ratings[1]));
+					setClarityRating(Number(ratings[2]));
+					console.log('Ratings received:');
+					console.log('Correctness:', ratings[0]);
+					console.log('Performance:', ratings[1]);
+					console.log('Clarity:', ratings[2]);
+
+					// Add delay before updating chart data
+					await new Promise(resolve => setTimeout(resolve, 200)); // 1000 ms = 1 second
+
+					// Update chart data after delay
+					setData([
+						{ activity: "Correctness", value: Number(ratings[0]), fill: "var(--color-Correctness)" },
+						{ activity: "Performance", value: Number(ratings[1]), fill: "var(--color-Performance)" },
+						{ activity: "Clarity", value: Number(ratings[2]), fill: "var(--color-Clarity)" },
+					]);
+				} else {
+					console.log('Unexpected response format:', ratingsText);
+				}
+			} else {
+				console.log('Response data is missing expected fields:', response.data);
+			}
+		} catch (error) {
+			console.log('Error while requesting ratings:', error);
+		} finally {
+			setGeneratingAnswer(false);
+		}
+	};
+
+	const handleSubmit = () => {
+		if (passedTests === form.testCases.length) {
+			toast.success('Congratulations! All test cases are passed! 🎉🎉🎉');
+			requestRating();
+			// requestCodeReview();
+		} else {
+			toast.error(
+				`You passed ${passedTests} out of ${form.testCases.length} test cases.`,
+			);
+		}
+	};
+
+	// const requestCodeReview = async () => {
+	// 	const sourceCode = editorRef.current.getValue();
+	// 	setGeneratingAnswer(true);
+	// 	console.log('Requesting code review...');
+
+	// 	try {
+	// 		const response = await axios({
+	// 			url: `https://generativelanguage.googleapis.com/v1beta/models/gemini-pro:generateContent?key=${import.meta.env.VITE_API_GENERATIVE_LANGUAGE_CLIENT}`,
+	// 			method: 'post',
+	// 			data: {
+	// 				contents: [
+	// 					{
+	// 						parts: [
+	// 							{
+	// 								text: `Review the following code and provide a summary including the advantages, disadvantages, and suggestions for improvement.\n\nCode:\n${sourceCode}\n\nPlease format the response as follows:\n- Advantages:...\n- Disadvantages:...\n- Suggestions:...\n`,
+	// 							},
+	// 						],
+	// 					},
+	// 				],
+	// 			},
+	// 		});
+
+	// 		const reviewText = response.data.candidates[0]?.content?.parts[0]?.text?.trim();
+	// 		if (reviewText) {
+	// 			// Example of splitting the review text if it's formatted with newlines
+	// 			const [advantages, disadvantages, suggestions] = reviewText.split('\n- ').map(part => part.trim()).filter(Boolean);
+
+	// 			// Validate and format the review data
+	// 			const isValid = (text) => text && typeof text === 'string' && text.trim().length > 0;
+
+	// 			const formattedAdvantages = isValid(advantages) ? advantages : 'No pros about the code.';
+	// 			const formattedDisadvantages = isValid(disadvantages) ? disadvantages : 'No cons about the code.';
+	// 			const formattedSuggestions = isValid(suggestions) ? suggestions : 'No suggestions about the code.';
+
+	// 			console.log('Code Review Received:');
+	// 			console.log('Advantages:', formattedAdvantages);
+	// 			console.log('Disadvantages:', formattedDisadvantages);
+	// 			console.log('Suggestions:', formattedSuggestions);
+
+	// 			// Update UI with the review data if necessary
+	// 			setAdvantages(formattedAdvantages);
+	// 			setDisadvantages(formattedDisadvantages);
+	// 			setSuggestions(formattedSuggestions);
+	// 		} else {
+	// 			console.log('Response data is missing expected fields:', response.data);
+	// 		}
+	// 	} catch (error) {
+	// 		console.log('Error while requesting code review:', error);
+	// 	} finally {
+	// 		setGeneratingAnswer(false);
+	// 	}
+	// };
+
+	// // Define initial values for advantages, disadvantages, and suggestions
+	// const DEFAULT_PROS = 'No advantages about the code.';
+	// const DEFAULT_CONS = 'No disadvantages about the code.';
+	// const DEFAULT_SUGGESTIONS = 'No suggestions about the code.';
+
+	// // Example state management using React's useState hook
+	// const [advantages, setAdvantages] = useState(DEFAULT_PROS);
+	// const [disadvantages, setDisadvantages] = useState(DEFAULT_CONS);
+	// const [suggestions, setSuggestions] = useState(DEFAULT_SUGGESTIONS);
+
 	return (
 		<div className='flex flex-col w-full mt-4'>
 			<div className='flex justify-between items-center mb-2'>
@@ -123,27 +315,83 @@ const Output = ({ editorRef, language }) => {
 				>
 					Run Code
 				</button>
-				<button
-					className={`px-4 py-2 rounded-md text-white ${
-						passedTests === form.testCases.length
-							? 'bg-green-500'
-							: 'bg-red-500'
-					} hover:bg-opacity-75 transition-all duration-300`}
-					disabled={!output}
-					onClick={() => {
-						if (passedTests === form.testCases.length) {
-							toast.success(
-								'Congratulations! All test cases are passed! 🎉🎉🎉',
-							);
-						} else {
-							toast.error(
-								`You passed ${passedTests} out of ${form.testCases.length} test cases.`,
-							);
-						}
-					}}
-				>
-					Submit ({passedTests}/{form.testCases.length})
-				</button>
+				<Dialog>
+					<DialogTrigger asChild>
+						<Button
+							className={`px-4 py-2 rounded-md text-white 
+				${passedTests === form.testCases.length ? 'bg-green-500' : 'bg-red-500'}
+			 `}
+							disabled={passedTests !== form.testCases.length}
+							onClick={handleSubmit}
+						>
+							Submit ({passedTests}/{form.testCases.length})
+						</Button>
+					</DialogTrigger>
+					<DialogContent className="sm:max-w-[425px]">
+						<DialogHeader>
+							<DialogTitle>Good work!</DialogTitle>
+							<DialogDescription>
+								Congratulations on acing the test! Your hard work and dedication have paid off. 🎉🎉🎉
+							</DialogDescription>
+						</DialogHeader>
+
+						<div className="grid gap-3">
+							<div className="flex justify-between items-center">
+								<Label htmlFor="content" className="flex-shrink-0">Overall Review</Label>
+								<TooltipProvider>
+									<Tooltip>
+										<TooltipTrigger asChild>
+											<Button variant="ghost" size="icon">
+												<Info className="size-4" />
+											</Button>
+										</TooltipTrigger>
+										<TooltipContent side="top">
+											{executionTime !== null ? `Run time: ${executionTime} ms` : 'No execution time yet'}
+										</TooltipContent>
+									</Tooltip>
+								</TooltipProvider>
+
+							</div>
+							<Card className="max-w">
+								<CardContent className="flex gap-4 p-4">
+									<div className="grid items-center gap-2">
+										{['Correctness', 'Performance', 'Clarity'].map((criteria, idx) => (
+											<div key={idx} className="grid flex-1 auto-rows-min gap-0.5">
+												<div className="text-sm text-muted-foreground">{criteria}</div>
+												<div className={`flex items-baseline gap-1 text-xl font-bold tabular-nums leading-none ${idx === 0 ? 'text-[#e88c30]' : idx === 1 ? 'text-[#2eb88a]' : 'text-[#2662d9]'}`}>
+													{criteria === 'Correctness' ? (correctnessRating !== null ? `${correctnessRating}/100` : 'N/A') :
+														criteria === 'Performance' ? (performanceRating !== null ? `${performanceRating}/100` : 'N/A') :
+															criteria === 'Clarity' ? (clarityRating !== null ? `${clarityRating}/100` : 'N/A') : 'N/A'}
+													<span className="text-sm font-normal text-muted-foreground">points</span>
+												</div>
+											</div>
+										))}
+									</div>
+									<ChartContainer
+										config={{
+											Correctness: { label: "Correctness", color: "hsl(var(--chart-3))" },
+											Performance: { label: "Performance", color: "hsl(var(--chart-2))" },
+											Clarity: { label: "Clarity", color: "hsl(var(--chart-1))" },
+										}}
+										className="mx-auto aspect-square w-full max-w-[80%]"
+									>
+										<RadialBarChart
+											margin={{ left: -10, right: -10, top: -10, bottom: -10 }}
+											data={data}
+											innerRadius="20%"
+											barSize={24}
+											startAngle={90}
+											endAngle={450}
+										>
+											<PolarAngleAxis type="number" domain={[0, 100]} dataKey="value" tick={false} />
+											<RadialBar dataKey="value" background cornerRadius={5} />
+										</RadialBarChart>
+									</ChartContainer>
+								</CardContent>
+							</Card>
+						</div>
+					</DialogContent>
+				</Dialog>
 			</div>
 
 			{isError && (
@@ -151,9 +399,7 @@ const Output = ({ editorRef, language }) => {
 			)}
 
 			<div
-				className={`h-full p-2 rounded-md border-gray-300 border ${
-					isError ? 'border-red-500 text-red-400' : ''
-				}`}
+				className={`h-full p-2 rounded-md border-gray-300 border ${isError ? 'border-red-500 text-red-400' : ''}`}
 			>
 				{isLoading ? (
 					<div className='text-center mt-2'>
